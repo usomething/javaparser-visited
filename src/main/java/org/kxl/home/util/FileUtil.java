d1df277edbc81d141da2b5c87eed67698b338785
kk <kk@myautobest.com>
2025-02-20 09:54:24 +0800

    给lombok的Data、Getter、Setter注解安排get/set 方法，注意：这会修改源码，且无法搞定内部enum的类，要手动删除get/set方法

--- src/main/java/org/kxl/home/util/FileUtil.java
+++ src/main/java/org/kxl/home/util/FileUtil.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.RandomAccessFile;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,9 +29,17 @@ public class FileUtil {
             raf.setLength(0);
         }
         for (String line : lines) {
-            raf.writeBytes(line);
+            raf.writeBytes(new String(line.getBytes(StandardCharsets.UTF_8),StandardCharsets.ISO_8859_1));
         }
         raf.close();
+    }
+
+    public static void renameFile(String oldFile,String newFile){
+        new File(oldFile).renameTo(new File(newFile));
+    }
+
+    public static void deleteFile(String file){
+        new File(file).delete();
     }
 
     public static List<String> readFile(String file){
