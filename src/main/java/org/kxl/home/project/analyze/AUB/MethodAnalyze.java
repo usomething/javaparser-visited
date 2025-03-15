6d910a8e137c0094de4bd7d8f6ed3fe5dd2b92b4
kk <kk@myautobest.com>
2025-03-15 11:04:48 +0800

    去掉注释，没必要

--- src/main/java/org/kxl/home/project/analyze/AUB/MethodAnalyze.java
+++ src/main/java/org/kxl/home/project/analyze/AUB/MethodAnalyze.java
@@ -28,9 +28,7 @@ import java.util.stream.Collectors;
 public class MethodAnalyze {
 
 
-    private final static String root = "C:/workspace/OE/AutoBestChina/target/generated-sources/delombok"
-//            "C:/workspace/OE/AutoBestChina/src/main/java"
-            ;
+    private final static String root = "C:/workspace/OE/AutobestCheckout/target/generated-sources/delombok";
     private static String projectName = root.contains("AutoBestChina") ? "oe-admin" : root.contains("AutobestCheckout") ? "oe-online" : "unknow";
 
     private final static Boolean SHOW_DUPLICATED_METHOD_NAME = false;
